bfb53ad1e829783f8e98d668b355f19efc08252b
samweavers <weaverssam@yahoo.ca>
2024-11-09 23:37:44 -0500

    fix | redirent uri

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -29,7 +29,10 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams()
   params.append('client_id', clientId)
   params.append('response_type', 'code')
-  params.append('redirect_uri', 'http://localhost:5173/callback')
+  params.append(
+    'redirect_uri',
+    'https://spotify-fetch-top-items.pages.dev/callback'
+  )
   params.append('scope', 'user-read-private user-read-email user-top-read')
   params.append('code_challenge_method', 'S256')
   params.append('code_challenge', challenge)
@@ -67,7 +70,10 @@ export async function getAccessToken(clientId, code) {
   params.append('client_id', clientId)
   params.append('grant_type', 'authorization_code')
   params.append('code', code)
-  params.append('redirect_uri', 'http://localhost:5173/callback')
+  params.append(
+    'redirect_uri',
+    'https://spotify-fetch-top-items.pages.dev/callback'
+  )
   params.append('code_verifier', verifier)
 
   const result = await fetch('https://accounts.spotify.com/api/token', {
